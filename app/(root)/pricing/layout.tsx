e096e78bb9cbfb0998adc4295fab928876cacafa
Suhail <suhailcollegemail@gmail.com>
2024-04-28 12:10:43 +0530

    production

--- app/(root)/pricing/layout.tsx
+++ app/(root)/pricing/layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react'
 import { ThemeProvider } from "@/components/shared/Theme-provider"
-import { ModeToggle } from "@/components/shared/Theme-toggler"
+
 
 const layout = ({ children }: { children: ReactNode }) => {
     return (
